e83b8b4a8aa08bcc7523908b6d34ac75b367dbaa
pknujaewan <163826142+pknujaewan@users.noreply.github.com>
2024-05-27 20:15:53 +0900

    st

---
+++ jaewan/my-app/src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#N1">Home</a></li>
+          <li><a href="#N2">Idea</a></li>
+          <li><a href="#N3">SUBIN</a></li>
+          <li><a href="#N4">JAEWAN</a></li>
+          <li><a href="#N5">JIWON</a></li>
+          <li><a href="#N6">Skill</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
