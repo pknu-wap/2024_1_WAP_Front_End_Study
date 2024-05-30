ba7c7a8ba6304cd8e261fc169f5213978eca2dc7
daimlee <daim050822@gmail.com>
2024-05-30 14:30:28 +0900

    feat:main이랑 연결

--- LeeSuBin/mission4/src/styledComponents/mainbox/Main.js
+++ LeeSuBin/mission4/src/styledComponents/mainbox/Main.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useState} from 'react';
+import './Main.css';
+
 
 function Main({refs}) {
+  const [fadeIn, setFadeIn] = useState(false);
+
+   const handleButtonClick = () => {
+    setFadeIn(true);
+  };
+
   return (
     <main>
       <section id="main" ref={refs?.mainRef}>
@@ -13,11 +21,17 @@ function Main({refs}) {
         <br/>
         <br/>
         <br/>
-        <h2>Main</h2>
-        <p>Main</p>
+      </section>
+      <section id="home">
+      {!fadeIn && <button class='mainb' onClick={handleButtonClick}>Click!</button>}
+        <div className={`fade-in-section ${fadeIn ? 'visible' : ''}`}>
+          <h1>Wap Front end study PAGE</h1>
+          <p>Introduce my partner</p>
+        </div> 
       </section>
     </main>
   );
 }
 
 export default Main;
+
